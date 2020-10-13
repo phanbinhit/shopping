46df38e8f76bc4c74c12d914fbfeab92295bbd5a
phanbinh <phanbinh2671999@gmail.com>
2020-10-13 14:41:56 +0700

    Binh - create template express - 13.10.20

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000 | process.env.PORT;
+
+//router
+const indexRouter = require('./routers/index.route');
+
+//set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//set static folder
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+    console.log('Server listening on port:' + PORT);
+})
