afc683edd139ab66333387a3facb817455440059
Kien Hoang <htkien2511@gmail.com>
2020-10-30 11:10:01 +0700

    Kien - 30/10/2020 Add toast message add to cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -6,34 +6,47 @@ module.exports = {
         let users = await User.findById("5f9247eb873dd289ba2b9b32");
         let cart = users.cart;
         let products = [];
-        
+
         for (let product of cart) {
             let prt = await Product.findById(product.id);
             products.push(prt);
         }
-        
+
         res.render('pages/cart', {
             products: products
         });
-        
+
     },
     addCart: async (req, res, next) => {
         let data = req.query.product;
-        User.updateOne(
-            {
-                // id can sua
-                "_id": "5f9247eb873dd289ba2b9b32"
-            },
-            {
-                // xu ly trung cua number
-                "$push": { "cart": { "id": data.id, "number": data.number } }
+        let users = await User.findById("5f9247eb873dd289ba2b9b32");
+        let cart = users.cart;
+        
+        let ids = cart.map((item) => {
+            return item.id
+        })
 
-            },
-            function (err, docs) {
-                if (err) throw err;
-            }
-        );
-        res.locals.number = res.locals.number++;
+        let isSuccess = ids.indexOf(data.id)
+        console.log(isSuccess);
+        if (isSuccess == -1) {
+            User.updateOne(
+                {
+                    // id can sua
+                    "_id": "5f9247eb873dd289ba2b9b32"
+                },
+                {
+                    // xu ly trung cua number
+                    "$push": { "cart": { "id": data.id, "number": data.number } }
+
+                },
+                function (err, docs) {
+                    if (err) throw err;
+                }
+            );
+
+        }
+        res.send(isSuccess + "")
+        return
         res.redirect('/');
     }
 }
