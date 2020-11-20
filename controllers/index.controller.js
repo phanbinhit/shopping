bdd94182fcf0df9c959c3cce997fadb818bf71c5
phanbinh <phanbinh2671999@gmail.com>
2020-11-20 09:46:14 +0700

    Binh - fix linh tinh - 20.11.20

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,15 +1,18 @@
 const Product = require('../models/product.model');
 const User = require('../models/user.model');
+const Category = require('../models/category.model');
 
 module.exports = {
     index: async (req, res, next) => {
         let user = await User.findById("5f9247eb873dd289ba2b9b32");
         let number = user.cart.length;
+        let catelogy = await Category.find();
         
         let products = await Product.find();
         res.render('pages/index', { 
             products: products,
-            number: number
+            number: number,
+            catelogy: catelogy
          });
     },
     
